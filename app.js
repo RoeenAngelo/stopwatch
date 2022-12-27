fee9ad407bf112d48c341f063109a9ce75d501cb
Roeen <roeenacal@gmail.com>
2022-12-26 19:49:47 -0800

    Add HTML and CSS

---
+++ app.js
@@ -0,0 +1,7 @@
+const timer = document.querySelector('.timer');
+const startStopBtn = document.querySelector('#startStopBtn');
+const resetBtn = document.querySelector('#resetBtn');
+
+function stopWatch() {
+    let timer = 'O0:00:00'
+}
